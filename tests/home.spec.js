288ad5a9103b8793cfcdd1e11724caf28b3273a5
Amy Kapernick <amy@kapers.dev>
2024-08-26 05:43:06 +0800

    removed old content

--- tests/home.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('Expect home page to match screenshot', async ({ page }) => {
-	await page.goto('/')
-
-	await expect(page).toHaveScreenshot({ maxDiffPixels: 50 });
-})
