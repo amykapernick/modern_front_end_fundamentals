f268b50634de531e0c6cdc6ef2bade18d0c0c698
Amy Kapernick <amy@kapers.dev>
2025-05-19 12:30:34 +0800

    removed old stuff

--- tests/snapshot.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('Expect homepage screenshot to match snapshot', async ({ page }) => {
-	await page.goto('http://localhost:4321/')
-
-	await expect(page).toHaveScreenshot()
-})
